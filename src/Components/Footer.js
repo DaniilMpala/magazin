08abb98bfa60cc8d03946eeb2ae8fd7754a17888
Alexey <alex967268@gmail.com>
2021-08-03 13:54:24 +0300

    some commit

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,12 +1,11 @@
-import React from 'react'
-import { Link } from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
 
 class Footer extends React.Component {
     componentWillUnmount() {
         this.setState = (state, callback) => {
             return;
         };
-
     }
     render() {
         return (
@@ -14,8 +13,15 @@ class Footer extends React.Component {
                 <div>
                     <span>© 2021 SHOP-ACC. ALL RIGHT RESERVED</span>
                     <a href="https://freekassa.ru" target="_blank" rel="noopener noreferrer">
-                        <img src="https://cdn.freekassa.ru/banners/small-dark-1.png" title="Прием платежей на сайте" />
+                        <img
+                            src="https://cdn.freekassa.ru/banners/small-dark-1.png"
+                            title="Прием платежей на сайте"
+                        />
                     </a>
+                </div>
+
+                <div>
+                    Поддержка: <a href="mailto:support@dedrimmer.xyz">support@dedrimmer.xyz</a>
                 </div>
 
                 <div>
@@ -23,10 +29,8 @@ class Footer extends React.Component {
                     <Link to="/useful">Полезное</Link>
                 </div>
             </footer>
-        )
+        );
     }
 }
 
-export default Footer
-
-
+export default Footer;
