baa0c35cc146a9d5073b67ffa729997527297566
Alexey <alex967268@gmail.com>
2021-08-07 15:05:13 +0300

    item description

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-import { loginAdmin, loadnewItem, loadData, buyAcc, acceptbuyAcc,PayCheck } from './functional.js'
-import exp from 'express'
-import cookie from 'cookie'
-import path from 'path'
-import helmet from 'helmet'
-import rateLimit from "express-rate-limit"
-import ax from 'axios'
-
+import { loginAdmin, loadnewItem, loadData, buyAcc, acceptbuyAcc, PayCheck } from "./functional.js";
+import exp from "express";
+import cookie from "cookie";
+import path from "path";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
+import ax from "axios";
 
 var app_express = exp();
 
@@ -13,88 +12,106 @@ app_express.use(exp.json());
 app_express.use(exp.urlencoded({ extended: true }));
 
 app_express.use(helmet());
-app_express.use(helmet.contentSecurityPolicy({
-    directives: {
-        defaultSrc: ["'self'"],
-        styleSrc: ["*"],
-        scriptSrc: ["'self'", "'unsafe-inline'"],
-        frameSrc: ["'self'", "'unsafe-inline'"],
-        imgSrc: ["*"],
-        fontSrc: ["*"],
-    }
-}));
-
+app_express.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: ["'self'"],
+            styleSrc: ["*"],
+            scriptSrc: ["'self'", "'unsafe-inline'"],
+            frameSrc: ["'self'", "'unsafe-inline'"],
+            imgSrc: ["*"],
+            fontSrc: ["*"],
+        },
+    })
+);
 
 const apiLimiter = rateLimit({
     windowMs: 5 * 1000,
-    max: 150
+    max: 150,
 });
 
 app_express.use(apiLimiter);
 app_express.listen(1332);
-app_express.use('/', exp.static('./build'));
-app_express.get('*', async (req, res, next) => {
-    if (~[''].indexOf(req.path)) next()
+app_express.use("/", exp.static("./build"));
+app_express.get("*", async (req, res, next) => {
+    if (~[""].indexOf(req.path)) next();
     else {
-        res.sendFile(path.resolve('./build/index.html'), function (err) {
+        res.sendFile(path.resolve("./build/index.html"), function (err) {
             if (err) {
-                console.log(err)
+                console.log(err);
             }
-        })
+        });
     }
 });
-var payJson = {}
-app_express.post('/pushpay', async (req, res, next) => {
-    console.log('/pushpay')
-    console.log(req.body)
-    if (!~['168.119.157.136', '168.119.60.227', '138.201.88.124'].indexOf(req.headers['cf-connecting-ip'])) next()
+var payJson = {};
+app_express.post("/pushpay", async (req, res, next) => {
+    console.log("/pushpay");
+    console.log(req.body);
+    if (
+        !~["168.119.157.136", "168.119.60.227", "138.201.88.124"].indexOf(
+            req.headers["cf-connecting-ip"]
+        )
+    )
+        next();
 
     switch (req.body.us_url) {
-        case 'ezcush':
-            payJson[req.body.intid] = "https://" + req.body.us_url + "." + req.body.us_domen + "/Pay"
-            ax.post('https://ezcush.ru/pushfk', { id: req.body.MERCHANT_ORDER_ID, summa: req.body.AMOUNT, sign: req.body.SIGN })
+        case "ezcush":
+            payJson[req.body.intid] =
+                "https://" + req.body.us_url + "." + req.body.us_domen + "/Pay";
+            ax.post("https://ezcush.ru/pushfk", {
+                id: req.body.MERCHANT_ORDER_ID,
+                summa: req.body.AMOUNT,
+                sign: req.body.SIGN,
+            });
             break;
-        case 'shopacc':
-            payJson[req.body.intid] = "https://" + req.body.us_url + "." + req.body.us_domen + `/PayCheck?orderid=${req.body.MERCHANT_ORDER_ID}&email=${req.body.P_EMAIL}`
-            acceptbuyAcc()
+        case "shopacc":
+            payJson[req.body.intid] =
+                "https://" +
+                req.body.us_url +
+                "." +
+                req.body.us_domen +
+                `/PayCheck?orderid=${req.body.MERCHANT_ORDER_ID}&email=${req.body.P_EMAIL}`;
+            acceptbuyAcc();
             break;
     }
 
-    next()
-})
+    next();
+});
 
-app_express.post('/pay', async (req, res) => {
-    console.log('/pay', req.body, payJson)
-    if (payJson[req.body.intid]) res.redirect(payJson[req.body.intid])
-})
-app_express.post('/loadData', async (req, res) => {
-    res.json(await loadData())
-})
-app_express.post('/PayCheck', async (req, res) => {
-    res.json(await PayCheck(req.body))
-})
-app_express.post('/buyAcc', async (req, res) => {
-    res.json(await buyAcc(req.body))
-})
-app_express.post('/loadnewItem', async (req, res) => {
-    if (req.headers.cookie) res.json(await loadnewItem(cookie.parse(req.headers.cookie)['token'], req.body))
-    else return "Вы обходите систему, ай ай ай :)"
-})
+app_express.post("/pay", async (req, res) => {
+    console.log("/pay", req.body, payJson);
+    if (payJson[req.body.intid]) res.redirect(payJson[req.body.intid]);
+});
+app_express.post("/loadData", async (req, res) => {
+    res.json(await loadData());
+});
+app_express.post("/PayCheck", async (req, res) => {
+    res.json(await PayCheck(req.body));
+});
+app_express.post("/buyAcc", async (req, res) => {
+    res.json(await buyAcc(req.body));
+});
+app_express.post("/loadnewItem", async (req, res) => {
+    if (req.headers.cookie)
+        res.json(await loadnewItem(cookie.parse(req.headers.cookie)["token"], req.body));
+    else return "Вы обходите систему, ай ай ай :)";
+});
 
-app_express.post('/loginAdmin', async (req, res) => {
-    var responce = await loginAdmin(req.body.login, req.body.password, req.headers.cookie ? cookie.parse(req.headers.cookie)['token'] : "")
+app_express.post("/loginAdmin", async (req, res) => {
+    var responce = await loginAdmin(
+        req.body.login,
+        req.body.password,
+        req.headers.cookie ? cookie.parse(req.headers.cookie)["token"] : ""
+    );
     if (responce.succes) {
-        res.cookie('token', responce.responceCode, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true });
-        delete responce['responceCode']
-        res.json(responce)
+        res.cookie("token", responce.responceCode, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true });
+        delete responce["responceCode"];
+        res.json(responce);
     } else {
-        res.json(false)
+        res.json(false);
     }
-})
-console.log('loisten', 1332)
+});
+console.log("loisten", 1332);
 // app_express.post('/getRefferalCode', async (req, res) => {
 //     if (req.headers.cookie) res.json(await getRefferalCode(cookie.parse(req.headers.cookie)['X-CSRF-Token']))
 // })
-
-
-
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,35 +1,37 @@
-import React from 'react'
-import ax from 'axios'
+import React from "react";
+import ax from "axios";
 
 class Main extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
             modalWindowData: {},
             data: [],
             count: 1,
-            mail: ""
-        }
+            mail: "",
+        };
     }
     componentDidMount() {
-        ax.post('/loadData')
-            .then(({ data }) => {
-                this.setState({ data: data })
-            })
+        ax.post("/loadData").then(({ data }) => {
+            this.setState({ data: data });
+        });
     }
 
     Openbuy(id, id2) {
         this.setState({
-            modalWindowData: this.state.data.find(v => v.id == id).items.find(v => v.id == id2)
-        })
+            modalWindowData: this.state.data
+                .find((v) => v.id == id)
+                .items.find((v) => v.id == id2),
+        });
     }
 
     buy() {
-        ax.post('/buyAcc', { mail: this.state.mail, count: this.state.count })
-            .then(({ data }) => {
+        ax.post("/buyAcc", { mail: this.state.mail, count: this.state.count }).then(
+            ({ data }) => {
                 if (data.succes) window.location.href = data.url;
-                else this.props.notification('lose', data.messages)
-            })
+                else this.props.notification("lose", data.messages);
+            }
+        );
     }
 
     render() {
@@ -38,23 +40,24 @@ class Main extends React.Component {
                 <div className="headerMain">
                     <div class="headerMain__left-side">
                         <span className="titleSteam">Аккаунты Steam</span>
-                        <p>Добро пожаловать в магазин SHOP-ACC, где вы сможете купить аккаунты Steam с играми напрямую у игроков без переплат. Сделки защищены гарантом безопасности. Мы продаем аккаунты полученные только легальным путем.</p>
+                        <p>
+                            Добро пожаловать в магазин SHOP-ACC, где вы сможете купить
+                            аккаунты Steam с играми напрямую у игроков без переплат.
+                            Сделки защищены гарантом безопасности. Мы продаем аккаунты
+                            полученные только легальным путем.
+                        </p>
                     </div>
 
                     <div class="headerMain__right-side">
                         <div class="titleSteam__planet planet">
                             <div class="planet__row">
-                                <img class="planet-steam" src='img/Steam.svg' />
-                                <img class="planet-origin" src='img/origin.svg' />
-                                <img class="planet-uplay" src='img/uplay.svg' />
-                                <img class="planet-epic" src='img/epic.svg' />
+                                <img class="planet-steam" src="img/Steam.svg" />
+                                <img class="planet-origin" src="img/origin.svg" />
+                                <img class="planet-uplay" src="img/uplay.svg" />
+                                <img class="planet-epic" src="img/epic.svg" />
                             </div>
                         </div>
-
                     </div>
-
-
-
                 </div>
                 <div className="items">
                     {this.state.data.map((e, key) => {
@@ -63,53 +66,141 @@ class Main extends React.Component {
                                 <div className="title">
                                     <span>{e.title}</span>
                                 </div>
-                                {
-                                    e.items.map((v, key) =>
-                                        <div className="item" key={key}>
-                                            <div>
-                                                <img src={e.urlPhoto}></img>
-                                                <span>{v.title}</span>
-                                            </div>
-                                            <div>
-                                                <span className="colVo">{v.colVo} шт.</span>
-                                                <span>Цена за 1шт.</span>
-                                                <span className="colVo">{v.price}руб.</span>
-                                                <button data-bs-toggle="modal" data-bs-target="#Modal" onClick={this.Openbuy.bind(this, e.id, v.id)}>Купить</button>
+                                {e.items.map((v, key) => (
+                                    <div className="item" key={key}>
+                                        <div>
+                                            <img src={e.urlPhoto}></img>
+                                            <div class="item-title">
+                                                {v.title}
+                                                <div class="item-title__info">
+                                                    <p>{v.desc}</p>
+
+                                                    <p>
+                                                        После оплаты Вы получаете ссылку
+                                                        на скачивание архива, в котором
+                                                        лежат:
+                                                        <br />
+                                                        <br />
+                                                        1.&nbsp;Файл с данными, а именно:
+                                                        <br />
+                                                        &nbsp;
+                                                        &nbsp;&nbsp;&bull;&nbsp;&nbsp;логин
+                                                        и пароль от аккаунта
+                                                        <br />
+                                                        &nbsp;
+                                                        &nbsp;&nbsp;&bull;&nbsp;&nbsp;логин
+                                                        и пароль от родной почты
+                                                        <br />
+                                                        &nbsp;
+                                                        &nbsp;&nbsp;&bull;&nbsp;&nbsp;привязанный
+                                                        номер телефона и код из SMS
+                                                        <br />
+                                                        &nbsp;
+                                                        &nbsp;&nbsp;&bull;&nbsp;&nbsp;ссылка
+                                                        на профиль
+                                                        <br />
+                                                        <br />
+                                                        2.&nbsp;Файл&nbsp;для получения
+                                                        кодов из&nbsp;SteamGuard
+                                                        <br />
+                                                        3.&nbsp;Очень подробная инструкция
+                                                        по входу в аккаунт
+                                                    </p>
+                                                </div>
                                             </div>
                                         </div>
-                                    )
-                                }
-                            </>)
+                                        <div>
+                                            <span className="colVo">{v.colVo} шт.</span>
+                                            <span>Цена за 1шт.</span>
+                                            <span className="colVo">{v.price}руб.</span>
+                                            <button
+                                                data-bs-toggle="modal"
+                                                data-bs-target="#Modal"
+                                                onClick={this.Openbuy.bind(
+                                                    this,
+                                                    e.id,
+                                                    v.id
+                                                )}
+                                            >
+                                                Купить
+                                            </button>
+                                        </div>
+                                    </div>
+                                ))}
+                            </>
+                        );
                     })}
-                    <div class="modal fade" id="Modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div
+                        class="modal fade"
+                        id="Modal"
+                        tabindex="-1"
+                        aria-labelledby="exampleModalLabel"
+                        aria-hidden="true"
+                    >
                         <div class="modal-dialog">
                             <div class="modal-content">
                                 <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLabel">Оплата заказа</h5>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    <h5 class="modal-title" id="exampleModalLabel">
+                                        Оплата заказа
+                                    </h5>
+                                    <button
+                                        type="button"
+                                        class="btn-close"
+                                        data-bs-dismiss="modal"
+                                        aria-label="Close"
+                                    ></button>
                                 </div>
                                 <div class="modal-body">
                                     <div>
                                         <label>Почта</label>
-                                        <input value={this.state.mail} onChange={(e) => { this.setState({ mail: e.target.value }) }} placeholder="Ваша почта" />
+                                        <input
+                                            value={this.state.mail}
+                                            onChange={(e) => {
+                                                this.setState({ mail: e.target.value });
+                                            }}
+                                            placeholder="Ваша почта"
+                                        />
                                     </div>
                                     <div>
                                         <label>Количество</label>
-                                        <input value={this.state.count} onChange={(e) => { this.setState({ count: !isNaN(e.target.value) && e.target.value.trim() ? e.target.value : this.state.count }) }} />
+                                        <input
+                                            value={this.state.count}
+                                            onChange={(e) => {
+                                                this.setState({
+                                                    count:
+                                                        !isNaN(e.target.value) &&
+                                                        e.target.value.trim()
+                                                            ? e.target.value
+                                                            : this.state.count,
+                                                });
+                                            }}
+                                        />
                                     </div>
                                 </div>
                                 <div class="modal-footer">
-                                    <span>Итоговая сумма: {(this.state.count * this.state.modalWindowData.price).toFixed(2)} руб.</span>
-                                    <button onClick={this.buy.bind(this)} disabled={!this.state.mail} type="button">Купить</button>
+                                    <span>
+                                        Итоговая сумма:{" "}
+                                        {(
+                                            this.state.count *
+                                            this.state.modalWindowData.price
+                                        ).toFixed(2)}{" "}
+                                        руб.
+                                    </span>
+                                    <button
+                                        onClick={this.buy.bind(this)}
+                                        disabled={!this.state.mail}
+                                        type="button"
+                                    >
+                                        Купить
+                                    </button>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-
             </>
-        )
+        );
     }
 }
 
-export default Main
+export default Main;
